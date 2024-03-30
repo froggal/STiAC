129942f2095431a2ad6bc6aae56fd888ffa09f6d
froggal <keyfrog@outlook.kr>
2024-03-30 21:32:06 +0900

    🎨 Code fomating using Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,16 @@ export default function Home() {
         <h1>
           ST<span>i</span>AC
         </h1>
-        <h2>Study Timer Accuracy <br /> STiAC는 공부 시간 타이머와 정확도 계산기를 합친 형태의, 새로운 공부 시간 타이머입니다.</h2>
+        <h2>
+          Study Timer Accuracy <br /> STiAC는 공부 시간 타이머와 정확도 계산기를
+          합친 형태의, 새로운 공부 시간 타이머입니다.
+        </h2>
       </div>
       <div className={styles.launch}>
-        <h1>
-          현재 STiAC를 개발하고 있습니다. 곧 런칭될 예정입니다.
-        </h1>
-        <h2>런칭되면 해당 웹사이트에 바로 안내해드리겠습니다. 조금만 기다려주세요!</h2>
+        <h1>현재 STiAC를 개발하고 있습니다. 곧 런칭될 예정입니다.</h1>
+        <h2>
+          런칭되면 해당 웹사이트에 바로 안내해드리겠습니다. 조금만 기다려주세요!
+        </h2>
       </div>
       <Footer />
     </main>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,10 +2,13 @@ import Styles from './Footer.module.css';
 
 const Footer = () => {
     return (
-        <main>
-            <p className={Styles.footer}>STiAC - Study Timer Accuracy <br /> Copyright 2024 KeyFrog All rights reserved. <br /> sp.keyfrog@gmail.com</p>
-        </main>
-    )
+      <main>
+        <p className={Styles.footer}>
+          STiAC - Study Timer Accuracy <br /> Copyright 2024 KeyFrog All rights
+          reserved. <br /> sp.keyfrog@gmail.com
+        </p>
+      </main>
+    );
 }
 
 export default Footer
